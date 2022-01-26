eb0ed1f067155c094755d00d676459add6bf8831
Denis Davydenko <72747890+denisdavydenko@users.noreply.github.com>
2022-01-26 14:38:35 +0200

    add labels to deviceid&modelid

--- basestructures.go
+++ basestructures.go
@@ -12,8 +12,8 @@ type ibeamDeviceConfig interface {
 type BaseDeviceConfig struct {
 	Active      bool   `ibOrder:"1" ibDispatch:"active"`
 	Name        string `ibOrder:"5" ibDispatch:"name" ibDescription:""`
-	DeviceID    uint32 `ibOrder:"10" ibDispatch:"deviceid" ibValidate:"unique_inc" ibDescription:"Unique number identifier for this device"`
-	ModelID     uint32 `ibOrder:"15" ibDispatch:"modelid" ibDescription:"The model type of the device"`
+	DeviceID    uint32 `ibLabel:"Device Id" ibOrder:"10" ibDispatch:"deviceid" ibValidate:"unique_inc" ibDescription:"Unique number identifier for this device"`
+	ModelID     uint32 `ibLabel:"Model Id" ibOrder:"15" ibDispatch:"modelid" ibDescription:"The model type of the device"`
 	Description string `ibOrder:"20" ibDispatch:"description"`
 }
 
